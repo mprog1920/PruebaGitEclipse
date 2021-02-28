527b7268b256c97cb068a74dcfd9517d706c12ee
montsemsanz <montsemsanz@gmail.com>
2021-02-28 20:03:35 +0100

    Añadida println desde rama_desarrollo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Comentario nuevo en main
 		System.out.println("Hola mundo de nuevo");
+		System.out.println("Hola desde rama desarrollo");
 	}
 
 }
